6e613db6415fef3e298f3dd84bfc4df59c50f95e
Robert Wiewel <dev@ducktec.de>
2021-08-26 20:59:33 +0200

    Add upstream files from riscv-rt

---
+++ assemble.ps1
@@ -0,0 +1,16 @@
+# remove existing blobs because otherwise this will append object files to the old blobs
+Remove-Item -Force bin/*.a
+
+$crate = "riscv-rt"
+$extension_sets = @("i", "im", "ic", "imc")
+$pwd = Get-Location
+
+foreach ($ext in $extension_sets) {
+    riscv64-unknown-elf-gcc -ggdb3 -fdebug-prefix-map=$pwd=/riscv-rt -c -mabi=ilp32 -march=rv32$ext asm.S -o bin/$crate.o
+    riscv64-unknown-elf-ar crs bin/riscv32$ext-unknown-none-elf.a bin/$crate.o
+
+    riscv64-unknown-elf-gcc -ggdb3 -fdebug-prefix-map=$pwd=/riscv-rt -c -mabi=lp64 -march=rv64$ext asm.S -o bin/$crate.o
+    riscv64-unknown-elf-ar crs bin/riscv64$ext-unknown-none-elf.a bin/$crate.o
+}
+
+Remove-Item bin/$crate.o
---
+++ build.rs
@@ -0,0 +1,40 @@
+// NOTE: Adapted from cortex-m/build.rs
+extern crate riscv_target;
+
+use riscv_target::Target;
+use std::env;
+use std::fs;
+use std::io::Write;
+use std::path::PathBuf;
+
+fn main() {
+    let target = env::var("TARGET").unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let name = env::var("CARGO_PKG_NAME").unwrap();
+
+    if target.starts_with("riscv") {
+        let mut target = Target::from_target_str(&target);
+        target.retain_extensions("imc");
+
+        let target = target.to_string();
+
+        fs::copy(
+            format!("bin/{}.a", target),
+            out_dir.join(format!("lib{}.a", name)),
+        )
+        .unwrap();
+
+        println!("cargo:rustc-link-lib=static={}", name);
+        println!("cargo:rustc-link-search={}", out_dir.display());
+    }
+
+    // Put the linker script somewhere the linker can find it
+    fs::File::create(out_dir.join("link.x"))
+        .unwrap()
+        .write_all(include_bytes!("link.x"))
+        .unwrap();
+    println!("cargo:rustc-link-search={}", out_dir.display());
+
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=link.x");
+}
